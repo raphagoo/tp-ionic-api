85304065df93dc3c76af9bba7681cd4de131c996
raphagoo <raphael.agostini@ynov.com>
2021-11-26 16:46:40 +0100

    getsong with genius infos

--- src/controllers/geniusController.js
+++ src/controllers/geniusController.js
@@ -1,6 +1,6 @@
 import { verifyJwt } from '../services/jwtVerification.js';
 import { getSongById } from 'genius-lyrics-api';
-const CLIENT_TOKEN = 'eyniE5IjwThnas75VN8w0oCwESDZXu4rhA-FIfpN41ZJi6Vr2TwpI8RJsQA2frI2';
+const CLIENT_TOKEN = 'qavXPWKYl9ZF8c8RjJqrXfpOCzQNULCLqA4tizAJRzfLHbHYXi9uDG7eV7HfPxDu';
 import https from 'https'
 
 export const search = (req, res) => {
@@ -32,13 +32,38 @@ export const search = (req, res) => {
 };
 
 export const getSong = (req, res) => {
-   getSongById(req.params.id, CLIENT_TOKEN)
-       .then(song => {
-           res.status(200).send(song)
-       })
-       .catch(error => {
-           console.log(error)
-       })
+    let output = ''
+    const options = {
+        hostname: 'api.genius.com',
+        port: 443,
+        method: 'GET',
+        path: '/songs/' + req.params.id,
+        headers: {
+            Authorization: 'Bearer ' + CLIENT_TOKEN
+        }
+    }
+
+    const request = https.request(options, response => {
+        response.on('data', function (chunk) {
+            output += chunk
+        });
+        response.on('end', () => {
+            getSongById(req.params.id, CLIENT_TOKEN)
+                .then(song => {
+                    let json = {geniusInfos: JSON.parse(output), song: song}
+                    res.status(200).send(json)
+                })
+                .catch(error => {
+                    console.log(error)
+                })
+        });
+    })
+    request.on('error', error => {
+        res.sendStatus(500)
+    })
+
+    request.end();
+    output = '';
 };
 
 
